c2410e06c114fc8f31e1dc80abba771edd69aaf9
Srujana <1nt23ec156.srujana@nmit.ac.in>
2024-11-13 01:23:33 +0530

    Add files via upload

--- scenariobasedsll.c
+++ scenariobasedsll.c
@@ -1,110 +1,153 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/*
+Consider the SGPA scored by students which are in fractional format in subject C Programming 	Problem Solving during 1st semester. Write a modular C program to perform the following operations.
+	a) Create a  Singly Linked List to store SGPA of the student using Insert Front 	   operation.
+	b) Display all the students SGPA on the standard output device.
+	c) Count and Display SGPAs above 9.0.
+	d) Make a new Singly Linked List of SGPAs less than 4.0. (Original List of grades should be present without any changes).
 
-// Define the structure for plane information
-typedef struct Plane {
-    int id;
-    char country[31];
-    int capacity;
-    struct Plane* next;
-} Plane;
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-// Function to create a new plane node
-Plane* createPlane(int id, const char* country, int capacity) {
-    Plane* newPlane = (Plane*)malloc(sizeof(Plane));
-    newPlane->id = id;
-    strncpy(newPlane->country, country, 30);
-    newPlane->country[30] = '\0'; // Ensure null-termination
-    newPlane->capacity = capacity;
-    newPlane->next = NULL;
-    return newPlane;
+struct SGPA {
+    int rollno;
+    char name[30];
+    float SGPA;
+    struct SGPA *next;
+};
+
+struct SGPA *getnode() {
+    return (struct SGPA *)malloc(sizeof(struct SGPA));
 }
 
-// Function to insert a plane into the linked list
-void insertPlane(Plane** head, int id, const char* country, int capacity) {
-    Plane* newPlane = createPlane(id, country, capacity);
-    newPlane->next = *head;
-    *head = newPlane;
+void Readdata(struct SGPA *nw) {
+    printf("Enter the Roll No, Name, and SGPA: \n");
+    scanf("%d %s %f", &nw->rollno, nw->name, &nw->SGPA);
 }
 
-// Function to free the linked list
-void freeList(Plane* head) {
-    Plane* temp;
-    while (head != NULL) {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
+void displaydata(struct SGPA *nw) {
+    printf("%d\t%s\t%f\n", nw->rollno, nw->name, nw->SGPA);
 }
 
-// Function to process planes and categorize them
-void processPlanes(Plane* head, const char* cname, int* countTakeOff, int* countSecondRunway, Plane** secondRunwayHead, Plane** maxCapacityPlane) {
-    Plane* current = head;
-    *countTakeOff = 0;
-    *countSecondRunway = 0;
-    *maxCapacityPlane = NULL;
-    
-    while (current != NULL) {
-        if (strcmp(current->country, cname) == 0) {
-            (*countTakeOff)++;
+struct SGPA *createlistRight(int N) {
+    struct SGPA *nw, *prev, *first = NULL;
+    int i;
+    for (i = 0; i < N; i++) {
+        nw = getnode();
+        Readdata(nw);
+        nw->next = NULL;
+        if (first == NULL) {
+            first = nw;
         } else {
-            (*countSecondRunway)++;
-            insertPlane(secondRunwayHead, current->id, current->country, current->capacity);
-            if (*maxCapacityPlane == NULL || current->capacity > (*maxCapacityPlane)->capacity) {
-                *maxCapacityPlane = current;
-            }
+            prev->next = nw;
         }
-        current = current->next;
+        prev = nw;
+    }
+    return first;
+}
+
+struct SGPA *Insertfront(struct SGPA *first) {
+    struct SGPA *nw = getnode();
+    Readdata(nw);
+    nw->next = first;
+    return nw;
+}
+
+void displayall(struct SGPA *first) {
+    struct SGPA *curr = first;
+    if (curr == NULL) {
+        printf("\nList is empty\n");
+    } else {
+        while (curr != NULL) {
+            displaydata(curr);
+            curr = curr->next;
+        }
     }
 }
 
-// Function to print the linked list
-void printList(Plane* head) {
-    Plane* current = head;
-    while (current != NULL) {
-        printf("%d %s %d\n", current->id, current->country, current->capacity);
-        current = current->next;
+void SGPAabove9(struct SGPA *first) {
+    struct SGPA *curr = first;
+    while (curr != NULL) {
+        if (curr->SGPA > 9.0) {
+            displaydata(curr);
+        }
+        curr = curr->next;
     }
+}
+
+struct SGPA *SGPAbelow4(struct SGPA *first1) {
+    struct SGPA *newList = NULL, *nw, *last = NULL;
+    struct SGPA *curr = first1;
+    
+    while (curr != NULL) {
+        if (curr->SGPA < 4.0) {
+            nw = getnode();
+            nw->rollno = curr->rollno;
+            strcpy(nw->name, curr->name);
+            nw->SGPA = curr->SGPA;
+            nw->next = NULL;
+            
+            if (newList == NULL) {
+                newList = nw;
+            } else {
+                last->next = nw;
+            }
+            last = nw;
+        }
+        curr = curr->next;
+    }
+    
+    return newList;
 }
 
 int main() {
-    int N;
+    int ch, N;
+    struct SGPA *first1 = NULL, *first2 = NULL, *first3 = NULL;
+
+    printf("Enter number of nodes to create a list: ");
     scanf("%d", &N);
-    
-    Plane* planeList = NULL;
-    int id, capacity;
-    char country[31];
-    
-    // Reading plane information
-    for (int i = 0; i < N; i++) {
-        scanf("%d %s %d", &id, country, &capacity);
-        insertPlane(&planeList, id, country, capacity);
-    }
-    
-    // Read the destination country name
-    char cname[31];
-    scanf("%s", cname);
-    
-    int countTakeOff, countSecondRunway;
-    Plane* secondRunwayHead = NULL;
-    Plane* maxCapacityPlane = NULL;
-    
-    // Process the planes
-    processPlanes(planeList, cname, &countTakeOff, &countSecondRunway, &secondRunwayHead, &maxCapacityPlane);
-    
-    // Output results
-    printf("%d\n", countTakeOff);
-    printf("%d\n", countSecondRunway);
-    printList(secondRunwayHead);
-    
-    if (maxCapacityPlane != NULL) {
-        printf("%d %s %d\n", maxCapacityPlane->id, maxCapacityPlane->country, maxCapacityPlane->capacity);
-    }
-    
-    // Free allocated memory
-    freeList(planeList);
-    freeList(secondRunwayHead);
-    
-    return 0;
+    first1 = createlistRight(N);
+
+    do {
+        printf("\nENTER THE OPERATION TO PERFORM:\n");
+        printf("1] INSERT FRONT\n");
+        printf("2] DISPLAY ALL\n");
+        printf("3] DISPLAY SGPA ABOVE 9.0\n");
+        printf("4] SLL FOR SGPA BELOW 4.0\n");
+        printf("5] EXIT\n");
+        scanf("%d", &ch);
+
+        switch(ch) {
+            case 1: 
+                printf("Insert Front\n");
+                first1 = Insertfront(first1);
+                break;
+
+            case 2:
+                printf("Display All\n");
+                displayall(first1);
+                break;
+            
+            case 3:
+                printf("Display SGPA above 9.0\n");
+                SGPAabove9(first1);
+                break;
+            
+            case 4:
+                printf("SGPA below 4.0\n");
+                first3 = SGPAbelow4(first1);
+                displayall(first3);
+                break;
+
+            case 5:
+                break;
+
+            default:
+                printf("Invalid choice\n");
+                break;
+        }
+    } while(ch != 5);
+
+    return 0;
 }
