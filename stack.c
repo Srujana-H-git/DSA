b392a524a5db8d2104aaba5246e958d273e543bb
Srujana <1nt23ec156.srujana@nmit.ac.in>
2024-12-07 23:50:29 +0530

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,3 +1,13 @@
+struct Node
+{
+  int data;
+  struct node *next;
+};
+typedef struct Node *node;
+Node getnode()
+{ return malloc(sizeof(struct node));
+}
+
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
